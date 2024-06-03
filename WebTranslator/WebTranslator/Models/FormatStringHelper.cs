1e726e226d190a170ac998c6c5372f7eda054f17
Tryanks <tryanks@outlook.com>
2024-06-03 16:43:14 +0800

    Add FormatStringHelper.cs

---
+++ WebTranslator/WebTranslator/Models/FormatStringHelper.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace WebTranslator.Models;
+
+public static partial class FormatStringHelper
+{
+    private static readonly Regex FormatRegex = MyRegex();
+
+    public static FormatMode DetermineFormatMode(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return FormatMode.None;
+
+        var matches = FormatRegex.Matches(input);
+        if (matches.Count == 0)
+            return FormatMode.None;
+
+        foreach (Match match in matches)
+            if (match.Value.Contains('$'))
+                return FormatMode.Sorted;
+
+        return FormatMode.Format;
+    }
+
+    public static List<string> ExtractFormatStrings(string input)
+    {
+        var result = new List<string>();
+
+        if (string.IsNullOrEmpty(input))
+            return result;
+
+        var matches = FormatRegex.Matches(input);
+        foreach (Match match in matches) result.Add(match.Value);
+
+        return result;
+    }
+
+    [GeneratedRegex(@"%(\d+\$)?[a-hs%]", RegexOptions.Compiled)]
+    private static partial Regex MyRegex();
+}
+
+public enum FormatMode
+{
+    None,
+    Format,
+    Sorted
+}
