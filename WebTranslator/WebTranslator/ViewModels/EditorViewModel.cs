7b6b057b86b79198e52f72e831267b1a43e3dd11
Tryanks <tryanks@outlook.com>
2024-06-03 18:07:05 +0800

    Remove TabView

--- WebTranslator/WebTranslator/ViewModels/EditorViewModel.cs
+++ WebTranslator/WebTranslator/ViewModels/EditorViewModel.cs
@@ -1,22 +1,12 @@
-﻿using System.Collections.ObjectModel;
-using AvaloniaEdit.Utils;
-using ReactiveUI;
-using ReactiveUI.Fody.Helpers;
+﻿using ReactiveUI.Fody.Helpers;
 using WebTranslator.Models;
 
 namespace WebTranslator.ViewModels;
 
 public class EditorViewModel : ViewModelBase
 {
-    public EditorViewModel()
-    {
-        EditorPages.WhenAnyValue(x => x.Count)
-            .Subscribe(x => IsEmpty = x == 0);
-    }
-
     [Reactive] public bool IsEmpty { get; set; } = true;
-    [Reactive] public ObservableCollection<EditorContentViewModel> EditorPages { get; set; } = new();
-    [Reactive] public EditorContentViewModel? SelectedEditorPage { get; set; }
+    [Reactive] public EditorContentViewModel EditorContent { get; set; } = null!;
 
     public override void SetParameter(object? parameter)
     {
@@ -31,8 +21,7 @@ public class EditorViewModel : ViewModelBase
         else
             header = "New Tab";
         var page = new EditorContentViewModel(header, modDictionary);
-
-        EditorPages.Add(page);
-        SelectedEditorPage = page;
+        IsEmpty = false;
+        EditorContent = page;
     }
 }
