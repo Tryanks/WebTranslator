1ad72d8bcfc1e9a650f8d2f21ebb601161d32a58
Tryanks <tryanks@outlook.com>
2024-08-08 11:16:47 +0800

    Fix Window over transparent on Windows 10 or older

--- WebTranslator/WebTranslator/Views/MainWindow.axaml.cs
+++ WebTranslator/WebTranslator/Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Runtime.InteropServices;
 using Avalonia.Controls;
+using Avalonia.Media;
 
 namespace WebTranslator.Views;
 
@@ -7,5 +10,14 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        if (IsWindows11()) Background = Brushes.Transparent;
+        return;
+
+        bool IsWindows11()
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) return false;
+            return Environment.OSVersion.Version is { Major: >= 10, Build: >= 22000 };
+        }
     }
 }
